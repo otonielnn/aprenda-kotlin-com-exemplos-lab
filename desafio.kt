6243545327fb8318631e67e307a701d6d1e42700
otonielnn <otoniel_2002@live.com>
2024-01-04 03:00:30 -0300

    Finalizando desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,47 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+
+
+import Formacao// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, var idade : Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("O ${usuario.nome} foi Matriculado!!!")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val cursoDeRedes = ConteudoEducacional("Curso de Redes", 100, Nivel.DIFICIL)
+    val cursoDeKotlin = ConteudoEducacional("Curso de Kotlin", 80, Nivel.INTERMEDIARIO)
+    val cursoDePython = ConteudoEducacional("Curso de Python", 40, Nivel.BASICO)
+
+    val cienciaComputacao = Formacao("Ciência da Computação", listOf(cursoDeRedes, cursoDeKotlin, cursoDePython))
+
+    val aluno1 = Usuario("Otoniel", 21)
+    val aluno2 = Usuario("Jorge", 18)
+    val aluno3 = Usuario("Isabella", 23)
+
+    cienciaComputacao.matricular(aluno1)
+    cienciaComputacao.matricular(aluno2)
+    cienciaComputacao.matricular(aluno3)
+
+    println("\nBacharelado em ${cienciaComputacao.nome}")
+
+    println("\nCadeiras: ")
+    for (cadeira in cienciaComputacao.conteudos) {
+        println("${cadeira.nome} com duração de ${cadeira.duracao} horas - Nivel = ${cadeira.nivel}")
+    }
+
+    println("\nAlunos que cursam o Bacharelado em Ciência da Computação: ")
+    for (aluno in cienciaComputacao.inscritos) {
+        println(aluno.nome)
+    }
 }
